7a490cbb554b66eae79a20cdbe046e04629cadc3
David Gonzalez <josedavidgonzalezt@gmail.com>
2024-06-12 14:40:48 -0300

    fix env

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Accept',
   });
-  await app.listen(process.env.PORT ?? 8000);
+  await app.listen(process.env?.PORT || 8000);
 }
 bootstrap();
