6ed7765482c51a92e242eeaebd5ee874d25d8d29
David Gonzalez <josedavidgonzalezt@gmail.com>
2024-06-12 14:03:20 -0300

    adding env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RestaurantModule } from './modules/restaurant/restaurant.module';
 import { MongoModule } from './modules/moongose/mongoose.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [RestaurantModule, MongoModule],
+  imports: [ConfigModule.forRoot(), RestaurantModule, MongoModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/modules/moongose/mongoose.module.ts
+++ src/modules/moongose/mongoose.module.ts
@@ -2,10 +2,6 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://josedavidgonzalezt:21r2lDp4XgEeV667@cluster0.osbblgl.mongodb.net/easy-reserve?retryWrites=true&w=majority&appName=Cluster0',
-    ),
-  ],
+  imports: [MongooseModule.forRoot(process.env.MONGO_URL)],
 })
 export class MongoModule {}
